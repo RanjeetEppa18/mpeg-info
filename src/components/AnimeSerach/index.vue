a0a0d2dca666f309192cdd3b08f8ec0cdd3ee971
Ranjeet Eppakayala <ranjeet@vshsolutions.com>
2021-05-02 23:59:22 +0530

    anime-search

---
+++ src/components/AnimeSerach/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ hello }}</div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+export default {
+  setup() {
+    const hello = ref("hello");
+    onMounted(() => {
+      console.log("mounted in the composition api!");
+    });
+    return { hello };
+  },
+};
+</script>
+
+<style></style>
